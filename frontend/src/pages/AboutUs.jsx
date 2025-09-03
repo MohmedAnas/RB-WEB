b3505be4937a272e156603d63ed62fad333c7e21
MohmedAnas <143054234+MohmedAnas@users.noreply.github.com>
2025-09-03 18:53:42 +0530

    Update AboutUs.jsx

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from "react-router-dom";
 import gsap from 'gsap';
