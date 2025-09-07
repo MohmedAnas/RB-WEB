7c684079a268ffe22f559571f41ca312fd0570dc
MohmedAnas <143054234+MohmedAnas@users.noreply.github.com>
2025-09-07 17:55:26 +0530

    Update WakeUpScreen.jsx

--- frontend/src/components/WakeUpScreen.jsx
+++ frontend/src/components/WakeUpScreen.jsx
@@ -1,20 +1,28 @@
 // WakeUpScreen.jsx
 import React from "react";
 import "./WakeUpScreen.css"; // for CSS animation/styles
+import networkGif from "../assets/Network.gif"; // Adjust path based on your file location
 
 export default function WakeUpScreen({ onRetry }) {
   return (
     <div className="wakeup-screen-container">
-      {/* Running Mascot Animation (SVG or GIF) */}
       <div className="mascot-anim">
-        {/* Example: Replace with your SVG, Lottie, GIF, etc */}
-        <img alt="Mascot running" src="/assets/mascot-running.gif" width={120} />
+        {/* Use your animated GIF here */}
+        <img
+          alt="Server waking up animation"
+          src={networkGif}
+          width={140}
+          style={{ maxWidth: "90vw" }}
+        />
       </div>
       <div className="wakeup-messages">
         <h2>Server is waking up...</h2>
-        <p>Please wait while we connect to our backend.<br />This may take up to 30 seconds on first load.</p>
+        <p>
+          Please wait while we connect to our backend.
+          <br />
+          This may take up to 30 seconds on first load.
+        </p>
       </div>
-      {/* Fake/animated progress bar */}
       <div className="progress-bar-animated">
         <div className="progress-inner" />
       </div>
