068e6e373326d6900b1fe1f9ec2e7eb0589ce0b8
MohmedAnas <143054234+MohmedAnas@users.noreply.github.com>
2025-09-08 16:11:02 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Toaster } from "react-hot-toast";
